bbba25ead2d446eca5fae84c92c4f81f9281db07
Yudha <yudhapratama11005@gmail.com>
2026-02-18 00:51:50 +0700

    Adding Student File

--- lib/features/student/student_dashboard.dart
+++ lib/features/student/student_dashboard.dart
@@ -7,7 +7,7 @@ class StudentDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Student Dashboard')),
-      body: const Center(child: Text('Welcome to the Student Dashboard!')),
+      body: const Center(child: Text('INi nanti halaman student bang')),
     );
   }
 }
